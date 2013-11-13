e93dc6e83060dc8b91edb66011952efbd6fe73d5
Ben Cates <ben@ideum.com>
2013-11-13 08:36:04 -0700

    `bundle gem`

---
+++ lib/capistrano/rails/console.rb
@@ -0,0 +1,9 @@
+require "capistrano/rails/console/version"
+
+module Capistrano
+  module Rails
+    module Console
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/capistrano/rails/console/version.rb
@@ -0,0 +1,7 @@
+module Capistrano
+  module Rails
+    module Console
+      VERSION = "0.0.1"
+    end
+  end
+end
